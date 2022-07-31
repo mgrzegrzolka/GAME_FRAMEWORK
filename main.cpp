16a2ce744b40d623f7994d1a932368bca8a6ac8a
Mariusz Grzegrzółka <m.grzegrzolka@op.pl>
2022-07-31 08:40:22 +0200

    [TRAINING] - Chapter 4 - set FPS and delay time

--- main.cpp
+++ main.cpp
@@ -5,22 +5,31 @@
 #define SCREEN_WIDTH 640 
 #define SCREEN_HEIGHT 480
 
+const int FPS = 60;
+const int DELAY_TIME = 1000.0f / FPS; /* delay time in ms */
 
-int main(int argc, char** argv){
-    
+int main(int argc, char** argv)
+{
+    Uint32 frameStart, frameTime;    
 
-    if(!TheGame::Instance()->init("Chapter 1", 100, 100, SCREEN_HEIGHT, SCREEN_WIDTH, 0/*SDL_WINDOW_FULLSCREEN*/)){ 
+    if(!TheGame::Instance()->init("Chapter 1", 100, 100, SCREEN_HEIGHT, SCREEN_WIDTH, 0/*SDL_WINDOW_FULLSCREEN*/)) { 
         printf("Error: init() failed");
         return 0;
     }
     printf("Info: game init success!");
-    while(TheGame::Instance()->running()){
+    while(TheGame::Instance()->running()) {
+        frameStart = SDL_GetTicks();
 
         TheGame::Instance()->handleEvents();
         TheGame::Instance()->update();
         TheGame::Instance()->render();
 
-        SDL_Delay(10);
+        frameTime = SDL_GetTicks() - frameStart;
+
+        if(frameTime < DELAY_TIME)
+        {
+            SDL_Delay((int)(DELAY_TIME - frameTime));
+        }
     }
 
     TheGame::Instance()->clean();
